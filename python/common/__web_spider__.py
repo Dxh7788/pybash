14f8396a61b5e8e79428fc62569a41c1f287cb42
Dxh7788 <anan1215>
2018-01-24 19:52:15 +0800

    python #2 py爬虫2

--- python/common/__web_spider__.py
+++ python/common/__web_spider__.py
@@ -1,10 +1,8 @@
-import urllib.parse
+from urllib import parse, request, response
 
 url = 'http://www.server.com/login'
 user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
 values = {'username': 'cqc',  'password': 'XXXX'}
 headers = {'User-Agent': user_agent}
-data = urllib.parse.urlencode(values)
-request = urllib.request.Request(url, data, headers)
-response = urllib.request.urlopen(request)
-page = response.read()
+data = parse.urlencode(values)
+req = request.Request(url, data, headers)
