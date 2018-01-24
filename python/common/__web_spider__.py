7b11b9bcb553f42ec1d3f206a1626a96966e6c18
Dxh7788 <anan1215>
2018-01-24 19:26:13 +0800

    python #2 py爬虫

--- python/common/__web_spider__.py
+++ python/common/__web_spider__.py
@@ -1,4 +1,10 @@
-import urllib.request
+import urllib.parse
 
-response = urllib.request.urlopen("http://www.htouhui.com")
-print(response.read())
+url = 'http://www.server.com/login'
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+values = {'username': 'cqc',  'password': 'XXXX'}
+headers = {'User-Agent': user_agent}
+data = urllib.parse.urlencode(values)
+request = urllib.request.Request(url, data, headers)
+response = urllib.request.urlopen(request)
+page = response.read()
