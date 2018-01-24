f0e5f222f96cf24ad027a8e117ef843fb2e87caa
Dxh7788 <anan1215>
2018-01-24 20:00:44 +0800

    python #2 py爬虫2

--- python/common/__web_spider__.py
+++ python/common/__web_spider__.py
@@ -5,4 +5,5 @@ user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
 values = {'username': 'cqc',  'password': 'XXXX'}
 headers = {'User-Agent': user_agent}
 data = parse.urlencode(values)
+print(data)
 req = request.Request(url, data, headers)
