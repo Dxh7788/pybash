43348f5ef8f39915524591c927e44bc60666c16a
Dxh7788 <anan1215>
2018-01-25 21:45:05 +0800

    python #3 py学习成功案例

--- python/day02/__example__a__.py
+++ python/day02/__example__a__.py
@@ -1,5 +1,11 @@
 #
+from python.day02.demo.__python__class__def__ import DemoClass
 
 
 def __print__():
-    dc = DemoClass()
+    dc = DemoClass("ddx", 0.98)
+
+    dc.say_hello()
+
+
+__print__()
