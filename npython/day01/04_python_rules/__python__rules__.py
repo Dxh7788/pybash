c7e6545775cc4811f6b916c5e0beccb5dd0a30d8
Dxh7788 <anan1215>
2018-02-22 11:26:33 +0800

    python #201802221119 python学习二阶段之函数文档注释获取

--- npython/day01/04_python_rules/__python__rules__.py
+++ npython/day01/04_python_rules/__python__rules__.py
@@ -5,6 +5,12 @@ class MyAnnotation:
     文档字符串的规范:一行以句号, 问号或惊叹号结尾的概述(或者该文档字符串单纯只有一行). 接着是一个空行. 接着是文档字符串剩下的部分,
     它应该与文档字符串的第一行的第一个引号对齐
     """
+    def __init__(self):
+        pass
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
     def annotation(self):
         """
@@ -16,6 +22,6 @@ class MyAnnotation:
         print(self.__doc__)
 
 
-myAnnotation = MyAnnotation()
+myAnnotation = MyAnnotation("ddd", 12)
 print(myAnnotation.annotation())
 print(myAnnotation.annotation.__doc__)
