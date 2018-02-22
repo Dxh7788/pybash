3b813ab9befe5901f21f0b504a1466aa4cb7fa20
Dxh7788 <anan1215>
2018-02-22 15:22:05 +0800

    python #201802221135 python学习二阶段之数据类型

---
+++ npython/day01/05_vairables/python_variables.py
@@ -0,0 +1,15 @@
+# String,字符串
+line = '我是一个小小鸟'
+# numbers,包括整形(int、long)、浮点型float以及复数complex
+number = 1
+number0 = 1.5
+number1 = 1.00006
+
+# list 集合
+items = [1, 2, 3, 4, 5]
+# tuple元组,元组不能二次赋值，相当于一个只读列表
+tuples = (1, 2, [1, 2, 3])
+# Dictionary,字典
+dicts = {}
+dicts['one'] = 'This is one'
+dicts[2] = 'This is 2'
