1d55a50c477858c5ba97bd578e13f3c7bd47ef25
Dxh7788 <anan1215>
2018-02-23 14:17:49 +0800

    python #201802231156 python学习二阶段之百度图片抓取

--- npython/day02/baidu_pic_spider.py
+++ npython/day02/baidu_pic_spider.py
@@ -3,7 +3,24 @@
 # -*- coding:utf-8 -*-
 # Author:xiaohong.dong
 
+import requests
+import re
+import random
 url = ''
 
 if __name__ == '__main__':
-    print('主方法')
+    url = ('http://image.baidu.com/search/index?tn=baiduimage'
+           '&ipn=r&ct=201326592&cl=2&lm=-1&st=-1&fm=result&fr=&sf=1'
+           '&fmq=1519364542656_R&pv=&ic=0&nc=1&z=&se=1&showtab=0'
+           '&fb=0&width=&height=&face=0&istype=2&ie=utf-8&word=冲田杏梨')
+
+    html = requests.get(url)
+    print('解析冲田杏梨的图片....')
+    try:
+        for addr in re.findall('"objURL":"(.*?)"', html.text, re.S):
+            pics = requests.get(addr)
+            fb = open('E:\\data2\\img\\' + (str(random.randrange(0, 1000, 4))) + '.jpg', "wb")
+            fb.write(pics.content)
+            fb.close()
+    except Exception as e:
+        print(e)
