6c8a93953704ae8325465e667d4aa5fbba94238f
Dxh7788 <anan1215>
2018-02-23 16:40:56 +0800

    python #201802231156 python学习二阶段之内置函数

---
+++ npython/day02/objurl_parser.py
@@ -0,0 +1,8 @@
+#! /user/bin/python # 第一行是特殊注释行，称之为组织行，用来告诉GNU/Linux系统应该使用哪个解释器来执行。
+# Linux/Unix等要设置字符集，Windows不用设置
+# -*- coding:utf-8 -*-
+# Author:xiaohong.dong
+
+copyright()
+license()
+credits()
